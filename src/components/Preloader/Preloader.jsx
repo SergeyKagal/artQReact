7d3f143856633b89dc7f7631600c9fa95deb71bf
SergeyKagal <3461995@gmail.com>
2024-04-20 22:12:09 +0300

    feat: preloader done

--- src/components/Preloader/Preloader.jsx
+++ src/components/Preloader/Preloader.jsx
@@ -1,6 +1,8 @@
 import React from 'react';
 import './Preloader.css';
 
-export default function Preloader() {
-  return <div>Preloader</div>;
+export default function Preloader(props) {
+  console.log(props);
+  const { hide } = props;
+  return <div className={`preloader${hide ? ' hide' : ''}`}>LOADING...</div>;
 }
--- src/components/game/Game.jsx
+++ src/components/game/Game.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { setImgUrl } from './question-img-url';
 import './Game.css';
+import Preloader from '../Preloader/Preloader';
 
 const variants = [0, 1, 2, 3];
 
@@ -9,22 +10,25 @@ function Game(props) {
   const [questionNumberIndex, setQuestionNumberIndex] = useState(0);
   const { questionNumbers } = props;
   const [isImgLoaded, setImgLoaded] = useState(false);
-  console.log('category ', categoryNumber);
-  console.log(questionNumbers);
+
+  console.log(isImgLoaded);
   return (
     <div>
       <h3>Game</h3>
       <h4>Category number {categoryNumber + 1}</h4>
       <h4>Question number {questionNumberIndex + 1}</h4>
       <p>Question text {questionNumbers[questionNumberIndex]}</p>
+
       <img
-        className='question__image'
+        className={`question__image${isImgLoaded ? '' : ' hide'}`}
         src={setImgUrl(categoryNumber, questionNumbers[questionNumberIndex])}
         alt='question image'
         onLoad={() => {
           setImgLoaded(true);
         }}
       />
+      <Preloader hide={isImgLoaded} />
+
       <h4>Answers</h4>
       <ul className='answers'>
         {variants.map((variant) => (
